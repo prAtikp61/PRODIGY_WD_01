5f13702880e414837d3284be853cc3a72d80d59a
prAtikp61 <145778157+prAtikp61@users.noreply.github.com>
2024-03-05 00:40:43 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+function f1() {
+    document.querySelector("body").style.backgroundImage = "url('sky.JPG')";
+}
+
+function f2() { 
+    document.querySelector("body").style.backgroundImage = "url('img2.jpg')";  
+}
+
+function f3() {
+    document.querySelector("body").style.backgroundImage = "url('river.jpg')";
+}
+
+function f4() {
+    document.querySelector("body").style.backgroundImage = "url('mountain.jpg')";
+}
+
+function f5() {
+    document.querySelector("body").style.backgroundImage = "url('desert.jpg')";
+}
+
+function f6() {
+    document.querySelector("body").style.backgroundImage = "url('jungle.jpg')";
+}
